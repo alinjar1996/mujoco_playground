dbeb46aec3ae97a36d5f18ef501145bea0e460ab
alinjar1996 <alinjardannitdgp2014@gmail.com>
2026-04-03 20:29:01 +0300

    Training done, results not good. will retrain with more unroll length and num_timesteps and less discounting rewards.

--- mujoco_playground/config/manipulation_params.py
+++ mujoco_playground/config/manipulation_params.py
@@ -167,12 +167,12 @@ def brax_ppo_config(
     )
     rl_config.num_resets_per_eval = 1
   elif env_name == "DualUR5eBoxlift":
-    rl_config.num_timesteps = 5_000_000 #200_000_000
+    rl_config.num_timesteps = 10_000_000 #200_000_000
     rl_config.num_evals = 10 #20
     rl_config.num_minibatches = 32
-    rl_config.unroll_length = 20 #40
+    rl_config.unroll_length = 30 #40
     rl_config.num_updates_per_batch = 3 #4
-    rl_config.discounting = 0.99
+    rl_config.discounting = 0.97
     rl_config.learning_rate = 3e-4#3e-4
     rl_config.entropy_cost = 1e-2
     rl_config.num_envs = 512 #8192
